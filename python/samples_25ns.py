30fe31b82e83dbec3e2e30dec9bd4acb153a8bd0
Pablo de Castro <pablodecm@gmail.com>
2016-01-09 12:48:35 +0100

    add MC sample information

---
+++ python/samples_25ns.py
@@ -0,0 +1,59 @@
+
+
+mc_samples = {}
+
+mc_samples["HHTo4B_SM"] = {
+  "das_name" : "/GluGluToHHTo4B_node_SM_13TeV-madgraph/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/dallosso/hh2bbbb/non-resonant/analysis/13TeV/hh4bNores/data/Step0/tree_Step0_V13_SM.root",
+  "xs" : 34.3*0.58*0.58,
+  "gen_events" : 276432.  # should be 300k 
+}
+
+mc_samples["QCD_HT100to200"] = {
+  "das_name" : "/QCD_HT100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/dallosso/hh2bbbb/non-resonant/analysis/13TeV/hh4bNores/data/Step0/tree_Step0_V14_HT100-200.root",
+  "xs" : 27850000000.,
+  "gen_events" : 81637494.
+}
+
+mc_samples["QCD_HT200to300"] = {
+  "das_name" : "/QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/dallosso/hh2bbbb/non-resonant/analysis/13TeV/hh4bNores/data/Step0/tree_Step0_V14_HT200-300.root",
+  "xs" : 1717000000.,
+  "gen_events" : 18718905.
+}
+
+mc_samples["QCD_HT300to500"] = {
+  "das_name" : "/QCD_HT300to500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/dallosso/hh2bbbb/non-resonant/analysis/13TeV/hh4bNores/data/Step0/tree_Step0_V14_HT300-500.root",
+  "xs" : 351300000.,
+  "gen_events" :  19826197.
+}
+
+mc_samples["QCD_HT500to700"] = {
+  "das_name" : "/QCD_HT500to700_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/dallosso/hh2bbbb/non-resonant/analysis/13TeV/hh4bNores/data/Step0/tree_Step0_V14_HT500-700.root",
+  "xs" : 31630000.,
+  "gen_events" :  19664159.
+}
+
+mc_samples["QCD_HT700to1000"] = {
+  "das_name" : "/QCD_HT700to1000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/dallosso/hh2bbbb/non-resonant/analysis/13TeV/hh4bNores/data/Step0/tree_Step0_V14_HT700-1000.root",
+  "xs" : 6802000.,
+  "gen_events" :  15356448.
+}
+
+mc_samples["QCD_HT1000to1500"] = {
+  "das_name" : "/QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/dallosso/hh2bbbb/non-resonant/analysis/13TeV/hh4bNores/data/Step0/tree_Step0_V14_HT1000-1500.root",
+  "xs" : 1206000.,
+  "gen_events" :  4963895.
+}
+
+mc_samples["TTJets"] = {
+  "das_name" : "/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"
+  "lustre_path" : "/lustre/cmswork/dallosso/hh2bbbb/non-resonant/analysis/13TeV/hh4bNores/data/Step0/tree_Step0_V14_TTJets.root",
+  "xs" : 831760.,
+  "gen_events" : 11344206.
+}
