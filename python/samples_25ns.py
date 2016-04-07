9901067f8522304b2a3c67d7339efa8655d2797e
Pablo de Castro <pablodecm@gmail.com>
2016-04-07 16:20:50 +0200

    add QCD MuEnriched samples

--- python/samples_25ns.py
+++ python/samples_25ns.py
@@ -48,14 +48,14 @@ mc_samples["QCD_HT1000to1500"] = {
   "das_name" : "/QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM",     
   "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/tree_*.root",
   "xs" : 1207000.,
-  "gen_events" :  5049267. # failed jobs
+  "gen_events" :  4952356. # failed jobs
 }
 
 mc_samples["QCD_HT1500to2000"] = {
   "das_name" : "/QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM",     
   "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/tree_*.root",
   "xs" : 119900.,
-  "gen_events" : 3939077. # failed jobs
+  "gen_events" : 3851736. # failed jobs
 }
 
 mc_samples["QCD_HT2000toInf"] = {
@@ -66,10 +66,85 @@ mc_samples["QCD_HT2000toInf"] = {
 }
 
 mc_samples["TTJets"] = {
-  "das_name" : "/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM",
-  "lustre_path" : "/lustre/cmswork/dallosso/hh2bbbb/non-resonant/analysis/13TeV/hh4bNores/data/Step0/tree_Step0_V14_TTJets.root",
+  "das_name" : "/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM",
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/tree_*.root",
   "xs" : 831760.,
-  "gen_events" : 11344206.
+  "gen_events" : 10215131.
 }
 
+mc_samples["QCD_MuEnrichedPt5_Pt-20to30"] = {
+  "das_name" : "/QCD_Pt-20to30_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1//MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_Pt-20to30_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/tree_*.root",
+  "xs" : 2960198.4,
+  "gen_events" :  31680400.
+}
 
+mc_samples["QCD_MuEnrichedPt5_Pt-30to50"] = {
+  "das_name" : "/QCD_Pt-30to50_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1//MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_Pt-30to50_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/tree_*.root",
+  "xs" : 1652471.46,
+  "gen_events" :  29938361.
+}
+
+mc_samples["QCD_MuEnrichedPt5_Pt-50to80"] = {
+  "das_name" : "/QCD_Pt-50to80_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1//MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_Pt-50to80_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/tree_*.root",
+  "xs" : 437504.10,
+  "gen_events" :  20328105.
+}
+
+mc_samples["QCD_MuEnrichedPt5_Pt-80to120"] = {
+  "das_name" : "/QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1//MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/tree_*.root",
+  "xs" : 106033.66,
+  "gen_events" :  13749024.
+}
+
+mc_samples["QCD_MuEnrichedPt5_Pt-120to170"] = {
+  "das_name" : "/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1//MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/tree_*.root",
+  "xs" : 25190.52,
+  "gen_events" :  7971018.
+}
+
+mc_samples["QCD_MuEnrichedPt5_Pt-170to300"] = {
+  "das_name" : "/QCD_Pt-170to300_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1//MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_Pt-170to300_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/tree_*.root",
+  "xs" : 8654.49,
+  "gen_events" :  7910182.
+}
+
+mc_samples["QCD_MuEnrichedPt5_Pt-300to470"] = {
+  "das_name" : "/QCD_Pt-300to470_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1//MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_Pt-300to470_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/tree_*.root",
+  "xs" : 797.35,
+  "gen_events" :  7845620.
+}
+
+mc_samples["QCD_MuEnrichedPt5_Pt-470to600"] = {
+  "das_name" : "/QCD_Pt-470to600_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1//MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_Pt-470to600_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/tree_*.root",
+  "xs" : 79.03,
+  "gen_events" :  3746894.
+}
+
+mc_samples["QCD_MuEnrichedPt5_Pt-600to800"] = {
+  "das_name" : "/QCD_Pt-600to800_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1//MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_Pt-600to800_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/tree_*.root",
+  "xs" : 25.10,
+  "gen_events" :  3703331.
+}
+
+mc_samples["QCD_MuEnrichedPt5_Pt-800to1000"] = {
+  "das_name" : "/QCD_Pt-800to1000_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1//MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_Pt-800to1000_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/tree_*.root",
+  "xs" : 4.71,
+  "gen_events" :  3666110.
+}
+
+mc_samples["QCD_MuEnrichedPt5_Pt-1000toInf"] = {
+  "das_name" : "/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1//MINIAODSIM",     
+  "lustre_path" : "/lustre/cmswork/hh/VHBBHeppyV21/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/tree_*.root",
+  "xs" : 1.62,
+  "gen_events" :  3749244.
+}
