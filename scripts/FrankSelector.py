7c7197a7bc10b54923ca135bf4fdacc0a6a0990a
Pablo de Castro <pablodecm@gmail.com>
2016-06-01 14:19:13 +0200

    add TEventList so only events with 4+ tags are mixed

--- scripts/FrankSelector.py
+++ scripts/FrankSelector.py
@@ -4,7 +4,7 @@ import ROOT
 from ROOT import ThinEvent, ExtEvent
 from ROOT import TChain, FrankSelector,  vector
 from ROOT import TH1, TFile
-from ROOT import gInterpreter
+from ROOT import TEventList 
 
 from di_higgs.hh2bbbb.samples_25ns import mc_samples
 
@@ -36,10 +36,16 @@ mult = 5
 
 tc_hm = TChain("hem_tree")
 tc_hm.Add(thinEventPath+name+".root")
+tchain = TChain("tree")
+tchain.Add(thinEventPath+name+".root")
+
+min_n_CSVM = 4
+el = TEventList("el","el")
+tchain.Draw(">>el","Sum$(pfjets.getDiscriminatorC(\"CSV\") > 0.8) > {}".format(min_n_CSVM-1))
+
+tc_hm.SetEventList(el)
+tchain.SetEventList(el)
 selector = FrankSelector(ExtEvent(ThinEvent))(0, hlt_paths_v, isHH,
                                               isData, hlt_paths_or_v, tc_hm, mult)
-
-tchain = TChain("tree")
-n_added = tchain.Add(thinEventPath+name+".root")
 tchain.Process(selector, "ofile="+base_dir+name+".root", max_events)
 
